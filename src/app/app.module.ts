38ae9e8a67428a408343f6fe66445278d3b6db38
thai0521 <tnt00001@mix.wvu.edu>
2022-01-26 10:55:58 -0500

    more stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HeaderComponent } from './header/header.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
+
   ],
   imports: [
     BrowserModule
