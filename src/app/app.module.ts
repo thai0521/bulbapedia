b9cf2c0551a97fb3ab7940e356d4e9080a0840fc
thai0521 <tnt00001@mix.wvu.edu>
2022-01-26 21:41:54 -0500

    Starting footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HeaderComponent } from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
 
   ],
   imports: [
