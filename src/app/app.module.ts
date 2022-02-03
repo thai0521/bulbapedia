b93f7539ac9fe7bc549731aebf776f1d2718c2bf
thai0521 <tnt00001@mix.wvu.edu>
2022-02-03 07:01:41 -0500

    Button Groups

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { HeaderComponent } from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import{ButtonGroupComponent} from './buttonGroup/buttonGroup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    CardComponent
+    CardComponent,
+    ButtonGroupComponent
 
   ],
   imports: [
---
+++ src/app/buttonGroup/buttonGroup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'bp-buttonGroup',
+    templateUrl: 'buttonGroup.component.html',
+    styleUrls: ['buttonGroup.component.css'] 
+})
+export class ButtonGroupComponent{
+
+}
