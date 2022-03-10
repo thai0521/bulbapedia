ea223f99aeef557323cf507920a6c36a96fa989f
thai0521 <tnt00001@mix.wvu.edu>
2022-03-10 03:56:20 -0500

    Moved data to backend

--- src/app/card/archives-layout.component.ts
+++ src/app/card/archives-layout.component.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { BulbItemModel } from "../bulb-item.model";
 import { mock_bulb_list } from "../mock_bulb_list";
+import { BulbService } from "./bulbs.service";
 
 @Component({
     selector: 'bp-archives',
@@ -8,13 +9,19 @@ import { mock_bulb_list } from "../mock_bulb_list";
     styleUrls: ['archives-layout.component.css']
 })
 
-export class ArchivesLayoutComponent{
+export class ArchivesLayoutComponent implements OnInit{
     bulbs: BulbItemModel [] = [];
 
-    constructor(){
-      for(var bulb of mock_bulb_list){
-        console.log(bulb);
+    constructor(private service:BulbService){
+     
+    }
+  ngOnInit(): void {
+    console.log("Fetch Data");
+    this.service.getBulbs().subscribe(data => {
+      console.log(data);
+      for(var bulb of data){
         this.bulbs.push(bulb);
       }
-    }
+    });
+  }
 }
